24aba474192b84b4cc4a5088e63ecbf6500bad7f
Akib <akib.cmed@gmail.com>
2021-11-03 17:48:30 +0600

    Driver navigate and close

--- src/test/java/practice/selenium_practice/main.java
+++ src/test/java/practice/selenium_practice/main.java
@@ -5,17 +5,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //Creating webdriver
-        //Setdriver path
-//        System.setProperty("webdriver.chrome.driver", "F:\\Selenium Project\\Web Drivers\\chromedriver.exe");
-//        WebDriver Cdriver =new ChromeDriver();
-//        Cdriver.get("https://www.google.com/");
-//        System.out.println(Cdriver.getTitle());
+        //Set driver path
 
-        System.setProperty("webdriver.gecko.driver", "F:\\Selenium Project\\Web Drivers\\geckodriver.exe");
-        WebDriver Fdriver =new FirefoxDriver();
-        Fdriver.get("https://www.google.com/");
-        System.out.println(Fdriver.getTitle());
+
+        //        System.setProperty("webdriver.gecko.driver", "F:\\Selenium Project\\Web Drivers\\geckodriver.exe");
+//        WebDriver Fdriver =new FirefoxDriver();
+//        Fdriver.get("https://www.google.com/");
+//        System.out.println(Fdriver.getTitle());
+
+
+        System.setProperty("webdriver.chrome.driver", "F:\\Selenium Project\\Web Drivers\\chromedriver.exe");
+        WebDriver Cdriver =new ChromeDriver();
+        Cdriver.get("https://www.google.com/");
+        System.out.println(Cdriver.getTitle());
+        System.out.println(Cdriver.getCurrentUrl());
+        //System.out.println(Cdriver.getPageSource());
+
+        Cdriver.get("https://urban-cic-stag.firebaseapp.com/login");
+        Cdriver.navigate().back();
+        Cdriver.navigate().forward();
+        Cdriver.close(); //Closes current browser
+        //Cdriver.quit(); //Closes all browser
     }
 }
